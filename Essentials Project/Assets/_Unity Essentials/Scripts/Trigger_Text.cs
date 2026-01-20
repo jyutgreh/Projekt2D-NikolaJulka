34762a790ceab4fce751e2406659e24ea53e31fe
JuliaDuda <dudziari@gmail.com>
2026-01-20 01:21:13 +0100

    trigger z błędem

--- Essentials Project/Assets/_Unity Essentials/Scripts/Trigger_Text.cs
+++ Essentials Project/Assets/_Unity Essentials/Scripts/Trigger_Text.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Trigger_Text : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+  
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }  
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug Log("PODPOWIEDZ DOTYCZACY GRY);
     }
 }
