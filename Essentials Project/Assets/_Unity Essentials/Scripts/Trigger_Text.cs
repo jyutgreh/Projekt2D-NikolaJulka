f02f73f927ccfd63cf8a7b3194a5074edf40bd78
JuliaDuda <dudziari@gmail.com>
2026-01-20 02:03:31 +0100

    triggery

--- Essentials Project/Assets/_Unity Essentials/Scripts/Trigger_Text.cs
+++ Essentials Project/Assets/_Unity Essentials/Scripts/Trigger_Text.cs
@@ -1,21 +1,32 @@
+using TMPro;
 using UnityEngine;
 
 public class Trigger_Text : MonoBehaviour
 {
-  
+    public TextMeshProUGUI text;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
-    }  
 
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug Log("PODPOWIEDZ DOTYCZACY GRY);
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
     }
 }
